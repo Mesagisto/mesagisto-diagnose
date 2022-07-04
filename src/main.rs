7f77dbb00f3ca14a7f58ba0040cc92237ccf2357
Itsusinn <itsusinn@meowcat.org>
2022-07-04 17:32:08 +0800

    feat: fetch upstream

--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,64 @@
 #![allow(incomplete_features)]
 #![feature(backtrace, capture_disjoint_fields)]
 
-
 use arcstr::ArcStr;
-use env_logger::TimestampPrecision;
 use futures_util::FutureExt;
-use log::info;
-use mesagisto_client::{EitherExt, MesagistoConfig};
-use mesagisto_client::data::{Packet, message};
 use mesagisto_client::data::message::{MessageType, Profile};
+use mesagisto_client::data::{message, Packet};
 use mesagisto_client::server::SERVER;
+use mesagisto_client::{EitherExt, MesagistoConfig};
+use tokio::io::{BufReader, AsyncBufReadExt};
+use tracing::{info, trace, warn, Level};
+use tracing_subscriber::{prelude::__tracing_subscriber_SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
-async fn main() -> anyhow::Result<()> {
-
+async fn main() {
   std::env::set_var("RUST_BACKTRACE", "full");
-  env_logger::builder()
-    .write_style(env_logger::WriteStyle::Auto)
-    .filter(None, log::LevelFilter::Error)
-    .format_timestamp(None)
-    .filter(Some("mesagisto_diagnose"), log::LevelFilter::Trace)
-    .filter(Some("mesagisto_client"), log::LevelFilter::Trace)
+  tracing_subscriber::registry()
+    .with(
+      tracing_subscriber::fmt::layer()
+        .with_target(true)
+        .with_timer(tracing_subscriber::fmt::time::OffsetTime::new(
+          // use local time
+          time::UtcOffset::__from_hms_unchecked(8, 0, 0),
+          time::macros::format_description!(
+            "[year repr:last_two]-[month]-[day] [hour]:[minute]:[second]"
+          ),
+        )),
+    )
+    .with(
+      tracing_subscriber::filter::Targets::new()
+        .with_target("mesagisto_diagnose", Level::TRACE)
+        .with_target("mesagisto_client", Level::TRACE)
+        .with_default(Level::WARN),
+    )
     .init();
+  run().await.unwrap();
+}
 
-
-  let yes = vec!["yes","y"];
-
+async fn run() -> anyhow::Result<()> {
   info!("信使诊断工具启动中...");
-  info!("注: 有默认项时可输入Enter使用默认项.");
+  info!("注: 有默认项时可按下Enter使用默认项.");
   let mut line = String::new();
-  let cipher_enable:bool;
-  let cipher_key:String;
-  let refuse_plain:bool;
-  info!("是否启用加密 yes/no or y/n，默认yes");
-  next_line(&mut line).await?;
-  if yes.contains(&&*line.to_lowercase()) || line.to_lowercase() == "" {
-    cipher_enable = true;
-  } else {
-    cipher_enable = false;
-  }
-  if cipher_enable {
-    info!("请输入加密密钥");
-    next_line(&mut line).await?;
-    cipher_key = line.to_string();
-    info!("是否拒绝非加密消息 yes/no or y/n, 默认yes");
-    next_line(&mut line).await?;
-    if yes.contains(&&*line.to_lowercase()) || line.to_lowercase() == "" {
-      refuse_plain = true;
-    } else {
-      refuse_plain = false;
-    }
-  } else {
-    cipher_key = String::new();
-    refuse_plain = false;
-  }
-  info!("请输入服务器地址, 默认 nats://itsusinn.site:4222");
+  let cipher_key: String;
 
-  let server_addr:String;
+  info!("请输入加密密钥");
+  next_line(&mut line).await?;
+  cipher_key = line.to_string();
+  info!("请输入服务器地址, 默认 nats://nats.mesagisto.org:4222");
+  let server_addr: String;
   next_line(&mut line).await?;
   if line.to_lowercase() == "" {
-    server_addr = "nats://itsusinn.site:4222".to_string();
+    server_addr = "nats://nats.mesagisto.org:4222".to_string();
   } else {
     server_addr = line.trim().to_string();
   }
   MesagistoConfig::builder()
     .name("diagnose")
-    .cipher_enable(cipher_enable)
     .cipher_key(cipher_key)
-    .cipher_refuse_plain(refuse_plain)
     .proxy(None)
     .nats_address(server_addr)
-    .photo_url_resolver(|_| {
-      async {
-        anyhow::Result::Ok(ArcStr::new())
-      }.boxed()
-    })
+    .photo_url_resolver(|_| async { anyhow::Result::Ok(ArcStr::new()) }.boxed())
     .build()
     .apply()
     .await;
@@ -82,7 +66,13 @@ async fn main() -> anyhow::Result<()> {
   info!("请输入频道地址");
   next_line(&mut line).await?;
   let channel_addr = ArcStr::from(line.clone());
-  dbg!(channel_addr.clone());
+  let channel_addr_clone = channel_addr.clone();
+  tokio::task::spawn(async move {
+    SERVER
+      .recv("".into(), &channel_addr_clone, server_msg_handler)
+      .await
+      .unwrap()
+  });
 
   let profile = Profile {
     id: 0i64.to_be_bytes().into(),
@@ -100,7 +90,7 @@ async fn main() -> anyhow::Result<()> {
     reply: None,
   };
   let packet = Packet::from(message.tl())?;
-  SERVER.send_and_receive(vec![],channel_addr.clone(), packet, receive_from_server).await?;
+  SERVER.send(&"".into(), &channel_addr, packet, None).await?;
 
   loop {
     next_line(&mut line).await?;
@@ -121,28 +111,39 @@ async fn main() -> anyhow::Result<()> {
     };
     let packet = Packet::from(message.tl())?;
     info!("发送消息: {}", line);
-    SERVER.send_and_receive(vec![],channel_addr.clone(), packet, receive_from_server).await?;
+    SERVER.send(&"".into(), &channel_addr, packet, None).await?;
   }
 }
 
-async fn next_line(buf: &mut String) -> async_std::io::Result<usize> {
+async fn next_line(buf: &mut String) -> tokio::io::Result<usize> {
   buf.clear();
-  let r = async_std::io::stdin().read_line(buf).await;
+  let mut stdin = BufReader::new(tokio::io::stdin());
+  let res = stdin.read_line(buf).await?;
   buf.remove(buf.len() - 1);
-  r
+  Ok(res)
 }
-pub async fn receive_from_server(message: nats::asynk::Message, _: Vec<u8>) -> anyhow::Result<()> {
-  let packet = Packet::from_cbor(&message.data)?;
+pub async fn server_msg_handler(
+  message: nats::asynk::Message,
+  _: ArcStr,
+) -> anyhow::Result<()> {
+  let packet = Packet::from_cbor(&message.data);
+  let packet = match packet {
+    Ok(v) => v,
+    Err(_e) => {
+      //todo logging
+      tracing::warn!("未知的数据包类型，请更新本消息源，若已是最新请等待适配");
+      return Ok(());
+    }
+  };
   match packet {
     either::Left(msg) => {
       info!("收到消息：");
-      dbg!(msg);
+      println!("{}", serde_json::to_string_pretty(&msg).unwrap());
     }
-    either::Right(_) => {
+    either::Right(event) => {
       info!("收到事件");
-      // dbg!(event);
+      println!("{:?}", serde_json::to_string_pretty(&event).unwrap());
     }
   }
   Ok(())
-
 }
